bb53283f1f488ee71ec25a850f7672342452a34c
Zuning Wang <wzuning@gmail.com>
2025-05-09 21:22:11 -0400

    Test git configuration for windows

--- tlias-web-management/src/main/java/com/itheima/mapper/DeptMapper.java
+++ tlias-web-management/src/main/java/com/itheima/mapper/DeptMapper.java
@@ -12,15 +12,19 @@ public interface DeptMapper {
     @Select("select id, name, create_time, update_time from dept order by update_time desc")
     List<Dept> findAll();
 
+    //根据id删除部门
     @Delete("delete from dept where id = #{id}")
     void deleteById(Integer id);
 
+    //添加部门
     @Insert("insert into dept (id, name, create_time, update_time) values (#{id}, #{name}, #{createTime}, #{updateTime})")
     void insert(Dept dept);
 
+    //回显部门
     @Select("select id, name, create_time, update_time from dept where id = #{id}")
     Dept getById(Integer id);
 
+    //修改部门
     @Update("update dept set id = #{id}, name = #{name}, update_time = #{updateTime} where id = #{id}")
     void update(Dept dept);
 }
